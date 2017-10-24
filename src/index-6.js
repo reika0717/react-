c1ec235a08f792af943586d64195e311c445f8f3
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-24 22:38:31 +0900

    no.4 -work3

---
+++ src/index-6.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+class Clock extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {time: new Date(), active: true}
+  }
+
+  componentDidMount () {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  // changeColor (){
+  //   setInterval(() => this.style={color: 'grey'}, 1000)
+  // }
+
+  componentWillUnMount () {
+    clearInterval(this.timerID)
+  }
+
+  tick () {
+    this.setState({time: new Date(), active: !this.state.active})
+  }
+
+  render () {
+    return (
+      <h1 style={this.state.active ? {color: 'grey'} : {color: 'black'}}>{this.state.time.toLocaleTimeString()}.</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+)
+
+/** Work
+1秒毎に色が black <-> grey と変わるようにして下さい
+*/
+
+// class Clock extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {time: new Date()}
+//   }
+//
+//   componentDidMount () {
+//     this.timerID = setInterval(() => this.tick(), 1000)
+//   }
+//
+//   componentWillUnMount () {
+//     clearInterval(this.timerID)
+//   }
+//
+//   tick () {
+//     this.setState({time: new Date()})
+//   }
+//
+//   render () {
+//     return (
+//       <h1 style={{color: 'grey'}}>{this.state.time.toLocaleTimeString()}.</h1>
+//     )
+//   }
+// }
+//
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+// )
--- src/index.js
+++ src/index.js
@@ -2,70 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-class Clock extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {time: new Date(), active: true}
-  }
+//二つある書き出し方の二つ目（extendを使ったやり方）　↔︎　No.2
+//superで親コンポーネントのコンストラクタを実行する
+//その場所はコンストラクタの中
+//constructorはnewした時に必ず実行されるメソッド　←JSの話
+//propsを使わない場合はconstructorを使用しなくてもいい
+//extendした場合はrenderメソッドを定義しなければいけない。その中でReactのコンポーネントを返してあげる。
 
-  componentDidMount () {
-    this.timerID = setInterval(() => this.tick(), 1000)
-  }
+// class Clock extends React.Component {
+//   //  constructor (props) {
+//   //    super(props)
+//   //  }
+//
+//   render () {
+//     const { current } = this.props //これもDeconstruct
+//     // 同じ　=> const time = this.props.time
+//     return (
+//       <h1>{current}.</h1>
+//     )
+//   }
+// }
 
-  // changeColor (){
-  //   setInterval(() => this.style={color: 'grey'}, 1000)
-  // }
-
-  componentWillUnMount () {
-    clearInterval(this.timerID)
-  }
-
-  tick () {
-    this.setState({time: new Date(), active: !this.state.active})
-  }
-
-  render () {
-    return (
-      <h1 style={this.state.active ? {color: 'grey'} : {color: 'black'}}>{this.state.time.toLocaleTimeString()}.</h1>
-    )
-  }
+const Clock = ({time}) =>{
+  return (
+    <h1>{time}</h1>    
+  )
 }
 
+/*
+実際はこんなイメージ。それをJSXで。
 ReactDOM.render(
-  <Clock />,
+  () => { new Clock({time: (new Date()).toLocaleTimeString()}) },
+  document.getElementById('root')
+)
+*/
+ReactDOM.render(
+  <Clock time={(new Date()).toLocaleTimeString()} />,
   document.getElementById('root')
 )
 
 /** Work
-1秒毎に色が black <-> grey と変わるようにして下さい
+• Clockコンポーネントが、props.time の代わりに props.current で受け取るようにしてください //定義と吐き出しのつながりを理解する
+• Clockコンポーネントのconstructorメソッドを削除したうえで、同じ表示になるようにして下さい  //データがどのように親コンポーネントから子コンポーネントに渡されているか理解する
+• Clockコンポーネントを、関数形式に書き変えて下さい
 */
-
-// class Clock extends React.Component {
-//   constructor (props) {
-//     super(props)
-//     this.state = {time: new Date()}
-//   }
-//
-//   componentDidMount () {
-//     this.timerID = setInterval(() => this.tick(), 1000)
-//   }
-//
-//   componentWillUnMount () {
-//     clearInterval(this.timerID)
-//   }
-//
-//   tick () {
-//     this.setState({time: new Date()})
-//   }
-//
-//   render () {
-//     return (
-//       <h1 style={{color: 'grey'}}>{this.state.time.toLocaleTimeString()}.</h1>
-//     )
-//   }
-// }
-//
-// ReactDOM.render(
-//   <Clock />,
-//   document.getElementById('root')
-// )
