f5356b6ed4e922453a96fd7905c222d9c4c2a518
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-10-24 20:50:18 +0900

    change color per second -step1

--- src/index.js
+++ src/index.js
@@ -5,24 +5,35 @@ import App from './App';
 class Clock extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {time: new Date()}
+    this.state = {time: new Date(), active: true}
   }
 
   componentDidMount () {
     this.timerID = setInterval(() => this.tick(), 1000)
   }
 
+  // changeColor (){
+  //   setInterval(() => this.style={color: 'grey'}, 1000)
+  // }
+
   componentWillUnMount () {
     clearInterval(this.timerID)
   }
 
   tick () {
-    this.setState({time: new Date()})
+    this.setState({time: new Date(), active: !this.state.active})
   }
 
   render () {
+    let color;
+    if(this.state.active === true){
+      color = {color: 'grey'}
+    }else {
+      color = {color: 'black'}
+    }
+
     return (
-      <h1 style={{color: 'grey'}}>{this.state.time.toLocaleTimeString()}.</h1>
+      <h1 style={color}>{this.state.time.toLocaleTimeString()}.</h1>
     )
   }
 }
@@ -35,3 +46,33 @@ ReactDOM.render(
 /** Work
 1秒毎に色が black <-> grey と変わるようにして下さい
 */
+
+// class Clock extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {time: new Date()}
+//   }
+//
+//   componentDidMount () {
+//     this.timerID = setInterval(() => this.tick(), 1000)
+//   }
+//
+//   componentWillUnMount () {
+//     clearInterval(this.timerID)
+//   }
+//
+//   tick () {
+//     this.setState({time: new Date()})
+//   }
+//
+//   render () {
+//     return (
+//       <h1 style={{color: 'grey'}}>{this.state.time.toLocaleTimeString()}.</h1>
+//     )
+//   }
+// }
+//
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+// )
