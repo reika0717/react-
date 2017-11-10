832f3163813a9521e2ec51af9b9e43acfa7ca313
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-11-10 21:24:16 +0900

    done tast

--- src/index-9.js
+++ src/index-9.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-class Root extends React.Component {
-  constructor (props) {
+class Clock extends React.Component{
+  constructor(props){
     super(props)
     this.state = {time: new Date()}
-  }
+}
 
   componentDidMount () {
     this.timerID = setInterval(() => this.tick(), 1000)
@@ -20,17 +20,28 @@ class Root extends React.Component {
     this.setState({time: new Date()})
   }
 
-  render () {
+  render(){
     return (
-      <div>
-        <h1>Hello React.</h1>
-        <Clock time={this.state.time.toLocaleTimeString()} />
-      </div>
+      <h1>{this.state.time.toLocaleTimeString()}.</h1>
     )
   }
 }
 
-const Clock = (props) => <h1>{props.time}.</h1>
+const Root = (props) => (
+  <div>
+    <h1>Hello React.</h1>
+    <Clock />
+  </div>
+)
+
+  //react コンポーネントオブジェクトを返す前になんらかの処理をしたい場合の書き方
+  // return {
+  //   処理
+  //   (<h1>{props.time}</h1>)
+  // }
+  // 実際のjsの処理(イメージ)
+  // return React.createElement(
+  //   <h1>{props.time}.</h1>
 
 ReactDOM.render(
   <Root />,
@@ -39,10 +50,11 @@ ReactDOM.render(
 
 /** MEMO: extends React.Component vs simple function
 stateを使用したい / componentDidMountなどのreactのライフサイクルフックを使用したい場合は
-extends React.Componentを使用してコンポーネントを定義します。
+extends React.Componentを使用してコンポーネントを定義します。→内部的に状態を持ちたいとき
 それ以外では極力関数の形式でコンポーネントを定義します。
 */
 
 /** Work
 書き換えの練習としてなにも参照せずに、ClockコンポーネントがRootコンポーネントの代わりにstateをもつように変更して下さい(※このような操作は本来は非推奨です)。
+上記の理由からconstで宣言したコンポーネントはstateを持てない → クラスとして書き換えるって意味の問題
 */
