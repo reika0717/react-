4eb2c7dcd8ab0d0a9440442233492af095fb3b79
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-11-10 21:21:33 +0900

    done task

--- src/index-8.js
+++ src/index-8.js
@@ -2,34 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const Root = () => (
+const Root = () =>(
   <div>
     <h1>Hello React.</h1>
     <Clock />
   </div>
 )
 
-class Clock extends React.Component {
-  constructor (props) {
+class Clock extends React.Component{
+  constructor(props){
     super(props)
-    this.state = {time: new Date()}
+    this.state = {time: new Date(), active: true}
   }
 
-  componentDidMount () {
-    this.timerID = setInterval(() => this.tick(), 1000)
-  }
+componentDidMount (){
+  this.timerID = setInterval(() => this.tick(),1000)
+  setInterval(() => this.changeColor(),3000)
+}
 
-  componentWillUnMount () {
-    clearInterval(this.timerID)
-  }
+componentWillUnMount (){
+  clearInterval(this.timerID)
+}
 
-  tick () {
-    this.setState({time: new Date()})
-  }
+tick (){
+  this.setState({time: new Date()})
+}
 
-  render () {
-    return (
-      <h1>{this.state.time.toLocaleTimeString()}.</h1>
+changeColor (){
+  this.setState({active: !this.state.active})
+}
+
+render(){
+    return(
+      <h1 style = {this.state.active ? {color: 'red'} : {color: 'black'}}>{this.state.time.toLocaleTimeString()}</h1>
     )
   }
 }
@@ -38,6 +43,7 @@ ReactDOM.render(
   <Root />,
   document.getElementById('root')
 )
+
 
 /** Work
 Rootコンポーネントの <h1> エレメントの文字色が、3秒おきに変わるようにして下さい。
